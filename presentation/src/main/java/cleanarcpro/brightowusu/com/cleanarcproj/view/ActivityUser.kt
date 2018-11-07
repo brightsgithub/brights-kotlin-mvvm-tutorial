8af9e395b72a29a07beb4659c82db12b0cd19fb9
brightsgithub <trainstationalert@gmail.com>
2018-11-07 12:01:31 +0000

    bottom nav added

--- presentation/src/main/java/cleanarcpro/brightowusu/com/cleanarcproj/view/ActivityUser.kt
+++ presentation/src/main/java/cleanarcpro/brightowusu/com/cleanarcproj/view/ActivityUser.kt
@@ -3,6 +3,7 @@ package cleanarcpro.brightowusu.com.cleanarcproj.view
 import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import cleanarcpro.brightowusu.com.cleanarcproj.MainApplication
 import cleanarcpro.brightowusu.com.cleanarcproj.R
 import cleanarcpro.brightowusu.com.cleanarcproj.di.components.DaggerActivityUserComponent
@@ -17,9 +18,30 @@ class ActivityUser : AppCompatActivity() {
     @Inject
     lateinit var displayUserViewModel: DisplayUserViewModel
 
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         initDependencies()
         initLiveDataObservers()
         loadUser()
